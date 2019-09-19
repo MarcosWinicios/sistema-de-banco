bcd7632af5a4a37e6c7fd46a019a1bf8e2eeb6da
marcosadf <marcos.filho18@gmail.com>
2019-09-19 01:19:37 -0300

    arquivos de front-end

---
+++ src/br/com/marcos/projetoweb/logica/AcessarClientePJ.java
@@ -0,0 +1,28 @@
+package br.com.marcos.projetoweb.logica;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.marcos.projetoweb.dao.PessoaJuridicaDAO;
+import br.com.marcos.projetoweb.model.Cliente;
+
+
+public class AcessarClientePJ implements Logica {
+	@Override
+	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		
+		String cnpj = req.getParameter("cnpj");
+		PessoaJuridicaDAO pjDAO = new PessoaJuridicaDAO(); 
+		
+		Cliente pj = pjDAO.pesquisarPj(cnpj);
+		
+		session.setAttribute("idCliente", pj.getIdCliente());
+		
+		return "cliente.jsp";
+	}
+}
