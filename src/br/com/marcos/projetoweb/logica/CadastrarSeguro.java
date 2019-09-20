da61726a6e027a4afeb9cd49bcbfbf1487962cd7
Marcos Winicios <pmarcoswinicios@gmail.com>
2019-09-19 21:47:10 -0300

    Cadastrar Seguto funcionando

--- src/br/com/marcos/projetoweb/logica/CadastrarSeguro.java
+++ src/br/com/marcos/projetoweb/logica/CadastrarSeguro.java
@@ -5,18 +5,24 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.marcos.projetoweb.dao.SeguroDAO;
+import br.com.marcos.projetoweb.model.Cliente;
 import br.com.marcos.projetoweb.model.Seguro;
 
 public class CadastrarSeguro implements Logica {
 
 	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		Cliente c = (Cliente) session.getAttribute("cliente");
+		
 		int numero = Integer.parseInt(req.getParameter("numApolice"));
-		//int situacao = Integer.parseInt(req.getParameter("situacao"));
 		double valor = Double.parseDouble(req.getParameter("valorSeguro"));
-		int idCliente = Integer.parseInt(req.getParameter("idCliente"));
+		
+		int idCliente = c.getIdCliente();
 		
 		Seguro seguro = new Seguro();
 		seguro.setNumero(numero);
